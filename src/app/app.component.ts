5d6ad76eedc9c2387f89b5c8089c5c2b946d634f
wisdomjhkwon <wisdomjhkwon@gmail.com>
2017-08-08 14:45:16 +0900

    일단 열심히 뭔가 수정함

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { Tutorial2Page } from '../pages/tutorial2/tutorial2'
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = Tutorial2Page;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,36 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { LongPressDirective } from '../directives/long-press/long-press';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Tutorial2Page } from "../pages/tutorial2/tutorial2";
+import { TextToSpeech } from "@ionic-native/text-to-speech";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Tutorial2Page,
+    LongPressDirective
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Tutorial2Page
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    TextToSpeech,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
