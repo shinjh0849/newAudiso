b939c8eb0f4dd7ef4c2d1bb5827bc4c3ed54b0cb
shinjh0849 <21200421@handong.edu>
2017-08-08 20:29:22 +0900

    앱모듈 정리 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
-import { RecommendPage } from './../pages/recommend/recommend';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Plugin
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
 
+//Directives
 import { DoubleTapDirective } from '../directives/double-tap/double-tap';
 import { ShortPressDirective } from '../directives/short-press/short-press';
 import { LongPressDirective } from '../directives/long-press/long-press';
+import { P1UpslidingDirective } from "../directives/p1-upsliding/p1-upsliding";
+import { T4ToRightDirective } from "../directives/t4-to-right/t4-to-right";
 
+//Pages
+import { SplashPage } from '../pages/splash/splash';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
-import { SplashPage } from '../pages/splash/splash';
 import { TutorialPage } from "../pages/tutorial/tutorial";
 import { Tutorial2Page } from "../pages/tutorial2/tutorial2";
+import { P1Page } from "../pages/p1/p1";
 import { ProductSizePage } from '../pages/product-size/product-size';
 import { ProductColorPage } from '../pages/product-color/product-color';
 import { ProductConfirmPage } from '../pages/product-confirm/product-confirm';
+import { RecommendPage } from './../pages/recommend/recommend';
 import { RecommendColorPage } from '../pages/recommend-color/recommend-color';
 import { RecommendMaterialPage } from '../pages/recommend-material/recommend-material';
 import { RecommendPricePage } from '../pages/recommend-price/recommend-price';
@@ -27,12 +34,6 @@ import { RecommendProductPage } from '../pages/recommend-product/recommend-produ
 import { NavigationPage } from '../pages/navigation/navigation';
 import { QRcodePage } from '../pages/q-rcode/q-rcode';
 import { ScanPage } from '../pages/scan/scan';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { P1UpslidingDirective } from "../directives/p1-upsliding/p1-upsliding";
-import { T4ToRightDirective } from "../directives/t4-to-right/t4-to-right";
-import { P1Page } from "../pages/p1/p1";
-
-
 
 
 
@@ -52,6 +53,7 @@ import { P1Page } from "../pages/p1/p1";
     HomePage,
     TutorialPage,
     Tutorial2Page,
+    P1Page,
     ProductColorPage,
     ProductConfirmPage,
     ProductSizePage,
@@ -63,7 +65,7 @@ import { P1Page } from "../pages/p1/p1";
     NavigationPage,
     ScanPage,
     QRcodePage,
-    P1Page
+    
   ],
   imports: [
     BrowserModule,
@@ -96,8 +98,9 @@ import { P1Page } from "../pages/p1/p1";
   providers: [
     StatusBar,
     SplashScreen,
-    TextToSpeech,
+    
     //Plugins
+    TextToSpeech,
     BarcodeScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
